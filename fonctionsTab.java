3a0a2669c219c08cd36cf94f5c4631e510090bfd
brahim <brahimbenamiraca@gmail.com>
2021-01-05 19:13:57 +0100

    Correction des commentaires (problème des accents)

--- fonctionsTab.java
+++ fonctionsTab.java
@@ -1,13 +1,13 @@
 package manipTab;
 import java.util.ArrayList;
 
-// D�finition de la classe fonctionsTab 
+// Définition de la classe fonctionsTab 
 // Celle-ci permet de manipuler un tableau(d'entiers) dynamique
 
 public class fonctionsTab {
 	
 	//Seul attribut de la classe
-	// Sur ce tableau, on va faire quelques manipulations � travers des m�thodes d�finies ci-dessous
+	// Sur ce tableau, on va faire quelques manipulations à travers des méthodes définies ci-dessous
 	private	ArrayList<Integer> tab;
 	
 	
@@ -27,50 +27,50 @@ public class fonctionsTab {
 	}
 	
 	
-	//M�thode permettant de rajouter un �lement � la fin du tableau
+	//Méthode permettant de rajouter un élement à la fin du tableau
 	public void ajoutElement(int elt) {
 		tab.add(elt);
 	}
 	
-	//M�thode permettant d'afficher les �lements du tableau
+	//Méthode permettant d'afficher les élements du tableau
 	public void affiche() {
-		System.out.println("Les �lements du tableau :");
+		System.out.println("Les élements du tableau :");
 		for(int i=0;i<tab.size();i++)
 			System.out.println("tab["+i+"]="+tab.get(i));
 	}
 	
-	//M�thode permettant de supprimer l'�lemenet du tableau dont l'indice est pass� en argument 
+	//Méthode permettant de supprimer l'élemenet du tableau dont l'indice est passé en argument 
 	public void suppElement(int i) {
-		// Si le tableau est vide, on  peut pas supprimer d'�lements
+		// Si le tableau est vide, on  peut pas supprimer d'élements
 		if (tab.size()==0)
-		System.out.println("Impossible d'effectuer l'op�ration. Le tableau est vide");// Affichage d'un message
+		System.out.println("Impossible d'effectuer l'opération. Le tableau est vide");// Affichage d'un message
 		
-		// Si l'indice pass� en argument est sup�rieur au nombre d'�lements du tableu, il est impossible
-		// d'�ffectuer la suppresion
+		// Si l'indice passé en argument est supérieur au nombre d'élements du tableu, il est impossible
+		// d'effectuer la suppresion
 		else if(i>=tab.size()) 
 			// Affichage d'un message
-			System.out.println("Impossible d'effectuer l'op�ration.\nL'indice saisi d�passe le nombre d'�lments du tableau");
-		else //sinon la suppresion est r�alis�e
+			System.out.println("Impossible d'effectuer l'opération.\nL'indice saisi dépasse le nombre d'élments du tableau");
+		else //sinon la suppresion est réalisée
 			tab.remove(i);
 	}
 	
-	//M�thode permettant de trier le tableau 
+	//Méthode permettant de trier le tableau 
 	public void tri() {
 		
 		
 		for(int i=0;i<tab.size();i++) {
-			// la variable ind_min d�signe l'indice du minimum de la partie du tableau commencant par l'�lement
-			// d'indice i jusqu'� la fin du tableau 
+			// la variable ind_min désigne l'indice du minimum de la partie du tableau commencant par l'élement
+			// d'indice i jusqu'à la fin du tableau 
 			int ind_min=i;
 			
-			// Cette boucle for servira � d�terminer ind_min
+			// Cette boucle for servira à déterminer ind_min
 			for(int j=i+1;j<tab.size();j++) {
 				if(tab.get(j)<tab.get(ind_min)) 
 					ind_min=j;
 			}
 			
-			// i�i on permute l'�lemnt d'indice i avec le minimum de la partie du tableau comman�ant par l'�l�ment
-			// d'indice i jusqu'� la fin
+			// içi on permute l'élement d'indice i avec le minimum de la partie du tableau commançant par l'élement
+			// d'indice i jusqu'à la fin
 			int temp=tab.get(i);
 			tab.set(i,tab.get(ind_min));
 			tab.set(ind_min,temp);
--- menuTableau.java
+++ menuTableau.java
@@ -10,53 +10,53 @@ public class menuTableau {
 		
 		
 		Scanner key=new Scanner(System.in);
-		int choix;// variable utilis�e dans le menu pour stocker le choix de l'utilisateur
+		int choix;// variable utilisée dans le menu pour stocker le choix de l'utilisateur
 		
 		System.out.println("Bienvenue dans le menu !\n");
 		
-		fonctionsTab t=new fonctionsTab();// d�claration d'un objet de type fonctionTab. Cet objet contient un seul
+		fonctionsTab t=new fonctionsTab();// déclaration d'un objet de type fonctionTab. Cet objet contient un seul
 		//attribut : un tableau d'entier dynamique
 		
-		do {// Pr�sentation des choix
+		do {// Présentation des choix
 			System.out.println("Saissez un entier pour choisir une option");
-			System.out.println("1- Ajouter un �lement au tableau");
+			System.out.println("1- Ajouter un élement au tableau");
 			System.out.println("2- Trier le tableau");
-			System.out.println("3- Supprimer un �lement du tableau");
-			System.out.println("4- Afficher les �lements du tableau\n");
+			System.out.println("3- Supprimer un élement du tableau");
+			System.out.println("4- Afficher les élements du tableau\n");
 			
 			choix=key.nextInt();// lecture du choix saisi par l'utilisateur
 			
-		// Selon la variable choix on appelle la m�thode de fonctionsTab
-		// Pensez � voir la d�finition de la classe fonctionTab pour voir les d�tails de chaque m�thode appel�e ci-dessous
+		// Selon la variable choix on appelle la méthode de fonctionsTab
+		// Pensez à voir la définition de la classe fonctionTab pour voir les détails de chaque méthode appelée ci-dessous
 		
 			switch (choix){
 			
-			case 1:// Ajouter un �lement � la fin du tableau
-				System.out.println("Veuillez saisir l'entier � ajouter au tableau");
+			case 1:// Ajouter un élement à la fin du tableau
+				System.out.println("Veuillez saisir l'entier à ajouter au tableau");
 				int elt=key.nextInt();//lecture de l'entier saisi
-				t.ajoutElement(elt);// appel de la m�thode ajoutElement permettant d'ajouter un �lement � la fin du tableau
+				t.ajoutElement(elt);// appel de la méthode ajoutElement permettant d'ajouter un élement à la fin du tableau
 				break;
 				
 			case 2:
-				t.tri();// m�thode permettant de trier le tableau
+				t.tri();// méthode permettant de trier le tableau
 				break;
 				
 			case 3:
-				System.out.println("Veuillez saisir l'indice de l'�lement du tableau � supprimer");
-				int i=key.nextInt();//Lecture de l'indice de l'�lement � virer du tableau
-				t.suppElement(i);// appel de la m�thode en question
+				System.out.println("Veuillez saisir l'indice de l'élement du tableau à supprimer");
+				int i=key.nextInt();//Lecture de l'indice de l'élement à virer du tableau
+				t.suppElement(i);// appel de la méthode en question
 				break;
 				
 			
 			case 4:
-				t.affiche();// m�thode permettant d'afficher les �lements du tableau
+				t.affiche();// méthode permettant d'afficher les élements du tableau
 				break;
 		}
 			
 			
 		System.out.println("Tapez 0 si vous voulez quitter. Sinon saisir autre chose");
 		choix=key.nextInt();
-		}while(choix!=0);// Apr�s l'�xecution de la m�thode associ�e au choix, on peut quitter le menu(sotir de la boucle do while)
+		}while(choix!=0);// Aprés l'éxecution de la méthode associée au choix, on peut quitter le menu(sotir de la boucle do while)
 		// il suffit juste de taper 0
 	}
 
